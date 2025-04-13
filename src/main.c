8f69e510cdeae17322eccc3a20d87eaa9e18796e
Oguz Bilgic <fisyonet@gmail.com>
2025-04-13 01:17:22 -0700

    Add all WIP changes

--- src/main.c
+++ src/main.c
@@ -1,22 +1,57 @@
+#include <stdio.h>
+
 #include "pico/stdlib.h"
+#include "hardware/uart.h"
+#include "hardware/irq.h"
 
-int main() {
-    const uint LED_PIN = 25;
+#define UART_ID uart0
+#define BAUD_RATE 9600
+#define UART_TX_PIN 0
+#define UART_RX_PIN 1
+#define MAX_MESSAGE_LENGTH 256
 
-    // Initialize the Pico SDK
-    stdio_init_all();
 
-    // Set the LED pin as an output
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
+void  main() {
+  gpio_init(25);
 
-    while (1) {
-        // Toggle the LED state
-        gpio_put(LED_PIN, !gpio_get(LED_PIN));
+  gpio_set_dir(25, GPIO_OUT);
 
-        // Delay for 500 milliseconds
-        sleep_ms(500);
+
+   stdio_init_all();
+
+    // Setup UART
+    uart_init(UART_ID, BAUD_RATE);
+
+    // Set the GPIO pins for UART
+    gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
+    gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
+
+        char buffer[MAX_MESSAGE_LENGTH];
+
+
+  while (true) {
+    gpio_put(25, !gpio_get(25));
+
+
+    // // Transmit a message via UART
+    // const char *message = "Hello from Pico!\n";
+    // uart_puts(UART_ID, message);
+
+
+    // Check if data is available
+    if (uart_is_readable(UART_ID)) {
+        // Read data into buffer
+        // uart_read_blocking
+         uart_read_blocking(UART_ID, buffer, 1);
+        
+        // Echo the received data back
+        // uart_write_blocking(UART_ID, buffer, length);
+        printf("%s", buffer);
+        sleep_ms(100);
+    } else {
+      printf("No data available\n");
+    // Wait for a bit
+    sleep_ms(1000);
     }
-
-    return 0;
+  }
 }
